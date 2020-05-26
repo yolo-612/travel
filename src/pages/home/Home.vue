dcc0a920cc93f4859d7de741d41bed4b8eb62b9a
keruisiya <2458097476@qq.com>
2020-05-26 23:38:18 +0800

    index-icon update

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,14 +2,14 @@
    <div>
        <home-header></home-header>
        <home-swiper></home-swiper>
-       
+       <home-icons></home-icons> 
    </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
-
+import HomeIcons from './components/Icons'
 
 
 
@@ -17,7 +17,8 @@ export default {
   name: 'Home',
   components:{
       HomeHeader,
-      HomeSwiper
+      HomeSwiper,
+      HomeIcons,
   }
 }
 </script>
